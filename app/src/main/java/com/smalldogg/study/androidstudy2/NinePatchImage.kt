6a5040b981847cac259b436d12813a3c1b3b2e79
small-dogg <sy920112@naver.com>
2023-05-14 21:03:36 +0900

    feat: 9patchImage

---
+++ app/src/main/java/com/smalldogg/study/androidstudy2/NinePatchImage.kt
@@ -0,0 +1,11 @@
+package com.smalldogg.study.androidstudy2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NinePatchImage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nine_patch_image)
+    }
+}
