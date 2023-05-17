f58446f18e69c1300321ddbd590a7d601b9ad898
small-dogg <sy920112@naver.com>
2023-05-17 20:24:35 +0900

    feat: 해상도별 이미지 지원

---
+++ app/src/main/java/com/smalldogg/study/androidstudy2/Resolution.kt
@@ -0,0 +1,11 @@
+package com.smalldogg.study.androidstudy2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Resolution : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resolution)
+    }
+}
